83e2f7089ca886a54b82dd72e5d0b02e4049a216
MatiasFigueroaUDC <matiigfigueroa@gmail.com>
2018-09-06 17:50:00 -0300

    Agrego las primeras 3 funciones de nuevo

--- LISTA.h
+++ LISTA.h
@@ -32,6 +32,27 @@ struct lista {
     Nodo *primerNodo;
 } typedef Lista;
 
+//N° 1
+// Precondición: Memoria disponible
+// Entrada:
+// Salida: Una lista
+// Postcondición: Se crea una lista en memoria
+Lista crearLista();
+
+//N° 2
+// Precondición:La lista tiene que ser válida
+// Entrada:Una lista
+// Salida:Un boolean(True:Está vacía/False:Contiene uno o más nodos)
+// Postcondición:Se determina si la lista está vacía o no
+int esListaVacia(Lista listaIngresada);
+
+//N° 3
+// Precondición:La lista tiene que ser válida y el nodo tiene que ser válido
+// Entrada: Una lista y un nodo
+// Salida: Un boolean(True:El nodo fue insertado con éxito al principio de la lista/False:error en la inserción)
+// Postcondición:Se agrega un nuevo nodo al inicio de la lista.
+int insertarNodoInicioDeLista(Lista listaIngresada, Nodo nuevoNodo);
+
 // 04  
 // Precondición:La lista tiene que ser válida, el nodo tiene que ser válido y la posición debe ser un entero positivo
 // Entrada: Una lista, un nodo y una posición
