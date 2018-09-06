8f547ec2e397ee29a6648683629d9720442cd27b
sdellatorre <sdellatorre@gmail.com>
2018-09-06 20:08:28 -0300

    Revert "cabeceras 4 a 6"
    This reverts commit 3d2611f9ed4824bf4597cc058f1490e3a8d0f353.

--- LISTA.h
+++ LISTA.h
@@ -1,8 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   LISTA.h
+ * Author: gonzalo
+ *
+ * Created on 6 de septiembre de 2018, 16:31
+ */
 
 #ifndef LISTA_H
 #define LISTA_H
 #define TRUE 1
 #define FALSE 0
+
 
 struct dato {
     int dato;
@@ -12,12 +25,12 @@ struct nodo {
     Nodo *anterior;
     Dato dato;
     Nodo *siguiente;
-} typedef *Nodo;
+} typedef Nodo;
 
 struct lista {
     int cantidadNodos;
     Nodo *primerNodo;
-} typedef *Lista;
+} typedef Lista;
 
 //N° 1
 // Precondición: Memoria disponible
@@ -45,9 +58,9 @@ int insertarNodoInicioDeLista(Lista listaIngresada, Nodo nuevoNodo);
 // Entrada: Una lista, un nodo y una posición
 // Salida: Un boolean (True:El nodo fue insertado con exito en la posicion indicada / False:error en la insercion)
 // Postcondición: Se agrega un nuevo nodo en la posicion indicada de la lista.
-int inserteNodoPosicionLista (Lista *cabecera, Nodo nuevoNodo, int posicion);
+int inserteNodoPosicionLista (Lista *cabecera, Nodo nuevoNodo);
 
-// 05
+//05
 // Precondición:La lista tiene que ser válida y el nodo tiene que ser válido 
 // Entrada: Una lista y un nodo
 // Salida: Un boolean (True:El nodo fue insertado con exito al final de la lista / False:error en la insercion)
