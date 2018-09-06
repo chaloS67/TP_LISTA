73d0a4dd5c4d7a470a1a4db4d469065c874bdf11
sdellatorre <sdellatorre@gmail.com>
2018-09-06 17:47:53 -0300

    funcion de leer Nodo en una posicion dada. fincion de leer Nodo la Final de la Lista. funcion de eliminar el primero de la Lista.

--- LISTA.h
+++ LISTA.h
@@ -53,5 +53,25 @@ void insertarNodoFinalLista (Lista *cabecera, Nodo nuevoNodo);
 // Postcondición: Se obtiene una copia del nodo que esta en la cabeza de la lista
 Nodo leerNodoInicioLista(Lista cabecera);
 
+// 07
+// Precondición:La lista tiene que ser válida y la posición debe ser un entero positivo
+// Entrada: Una lista y una posicion
+// Salida: Una copia del nodo
+// Postcondición: Se obtiene una copia del nodo según la posicion
+Nodo leer_Nodo_en_posicion_lista (Lista cabecera, int posicion);
+
+// 08
+// Precondición:La lista tiene que ser válida
+// Entrada: Una lista
+// Salida: Una copia del nodo
+// Postcondición: Se obtiene una copia del nodo ultimo de la lista
+Nodo leer_Nodo_final_lista (Lista cabecera);
+
+// 09
+// Precondición:La lista tiene que ser válida
+// Entrada: Una lista
+// Salida: Un boolean (TRUE:si se pudo eliminar nodo. FALSE: Si no se pudo eliminar nodo)
+// Postcondición:La lista sin el nodo de la posción dada.
+Lista eliminar_Nodo_inicio_lista (Lista *cabecera);
 #endif /* LISTA_H */
 
