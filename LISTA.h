d6c5e762faf5d7c8d9b2f5a53e713619c05a0c9c
clarkeen <40001889+clarkeen@users.noreply.github.com>
2018-09-11 18:51:33 -0300

    Update LISTA.h
    Actualisacion del archivo LISTA.h

--- LISTA.h
+++ LISTA.h
@@ -1,4 +1,3 @@
-
 /* 
  * File:   LISTA.h
  * Author: gonzalo
@@ -24,117 +23,89 @@ struct lista {
     Nodo primerNodo;
 } typedef *Lista;
 
-// 1
-// Precondici�n: Memoria disponible
+// 01
+// Precondición: Memoria disponible
 // Entrada:
 // Salida: Una lista
-// Postcondici�n: Se crea una lista en memoria
+// Postcondición: Se crea una lista en memoria
 Lista crearLista();
 
-// 2
-// Precondici�n:La lista tiene que ser v�lida
+// 02
+// Precondición:La lista tiene que ser válida
 // Entrada:Una lista
-// Salida:Un boolean(True:Est� vac�a/False:Contiene uno o m�s nodos)
-// Postcondici�n:Se determina si la lista est� vac�a o no
-
+// Salida:Un boolean(True:Está vacía/False:Contiene uno o más nodos)
+// Postcondición:Se determina si la lista está vacía o no
 bool esListaVacia(Lista cabecera);
 
 
-// 3
-// Precondici�n:La lista tiene que ser v�lida y el nodo tiene que ser v�lido
+// 03
+// Precondición:La lista tiene que ser válida y el nodo tiene que ser válido
 // Entrada: Una lista y un nodo
-<<<<<<< HEAD
-// Salida: Un boolean(True:El nodo fue insertado con �xito al principio de la lista/False:error en la inserci�n)
-// Postcondici�n:Se agrega un nuevo nodo al inicio de la lista.
-void insertarNodoInicioLista(Lista cabecera, Nodo nuevoNodo);
-=======
-// Salida: Un boolean(True:El nodo fue insertado con �xito al principio de la lista/False:error en la inserci�n)
-// Postcondici�n:Se agrega un nuevo nodo al inicio de la lista.
+// Salida: Un boolean(True:El nodo fue insertado con éxito al principio de la lista/False:error en la inserción)
+// Postcondición:Se agrega un nuevo nodo al inicio de la lista.
 void insertarNodoInicioLista(Lista *cabecera, Nodo nuevoNodo);
->>>>>>> 118b1d3368668f6e9747c889f3f415a35bb6b180
 
-// 4
-// Precondici�n:La lista tiene que ser v�lida, el nodo tiene que ser v�lido y la posici�n debe ser un entero positivo
-// Entrada: Una lista, un nodo y una posici�n
+// 04
+// Precondición:La lista tiene que ser válida, el nodo tiene que ser válido y la posición debe ser un entero positivo
+// Entrada: Una lista, un nodo y una posición
 // Salida: Un boolean (True:El nodo fue insertado con exito en la posicion indicada / False:error en la insercion)
-// Postcondici�n: Se agrega un nuevo nodo en la posicion indicada de la lista.
-
+// Postcondición: Se agrega un nuevo nodo en la posicion indicada de la lista.
 void insertarNodoEnPosicionLista(Lista cabecera, Nodo nuevoNodo, int posicion);
 
-// 5
-// Precondici�n:La lista tiene que ser v�lida y el nodo tiene que ser v�lido 
+// 05
+// Precondición:La lista tiene que ser válida y el nodo tiene que ser válido 
 // Entrada: Una lista y un nodo
 // Salida: Un boolean (True:El nodo fue insertado con exito al final de la lista / False:error en la insercion)
-// Postcondici�n: Se agrega un nuevo nodo en el final  de la lista.
-
+// Postcondición: Se agrega un nuevo nodo en el final  de la lista.
 void insertarNodoFinalLista(Lista *cabecera, Nodo nuevoNodo);
 
-// 6
-// Precondici�n:La lista tiene que ser v�lida 
+// 06
+// Precondición:La lista tiene que ser válida 
 // Entrada: Una lista
 // Salida: Un nodo False:error en la insercion)
-// Postcondici�n: Se obtiene una copia del nodo que esta en la cabeza de la lista
-
+// Postcondición: Se obtiene una copia del nodo que esta en la cabeza de la lista
 Nodo leerNodoInicioLista(Lista cabecera);
 
-
 // 07
-// Precondici�n:La lista tiene que ser v�lida y la posici�n debe ser un entero positivo
+// Precondición:La lista tiene que ser válida y la posición debe ser un entero positivo
 // Entrada: Una lista y una posicion
 // Salida: Una copia del nodo
-// Postcondici�n: Se obtiene una copia del nodo seg�n la posicion
+// Postcondición: Se obtiene una copia del nodo según la posicion
 Nodo leerNodoEnPosicionLista (Lista cabecera, int posicion);
 
 // 08
-// Precondici�n:La lista tiene que ser v�lida
+// Precondición:La lista tiene que ser válida
 // Entrada: Una lista
 // Salida: Una copia del nodo
-// Postcondici�n: Se obtiene una copia del nodo ultimo de la lista
+// Postcondición: Se obtiene una copia del nodo ultimo de la lista
 Nodo leerNodoFinalLista (Lista cabecera);
 
 // 09
-// Precondici�n:La lista tiene que ser v�lida
+// Precondición:La lista tiene que ser válida
 // Entrada: Una lista
 // Salida: Un boolean (TRUE:si se pudo eliminar nodo. FALSE: Si no se pudo eliminar nodo)
-<<<<<<< HEAD
-// Postcondici�n:La lista sin el nodo de la posci�n dada.
-int eliminarNodoInicioLista (Lista *cabecera);
-=======
-// Postcondici�n:La lista sin el nodo de la posci�n dada.
+// Postcondición:La lista sin el nodo de la posción dada.
 bool eliminarNodoInicioLista (Lista *cabecera);
->>>>>>> 118b1d3368668f6e9747c889f3f415a35bb6b180
 
 // 10
-// Precondici�n:La lista tiene que ser v�lida y la posici�n debe ser un entero positivo
+// Precondición:La lista tiene que ser válida y la posición debe ser un entero positivo
 // Entrada: Una lista y una posicion
 // Salida: Un boolean (TRUE:Si se borro un nodo FALSE : Si no se pudo borrar nodo
-<<<<<<< HEAD
-// Postcondici�n: Se obtiene una copia del nodo que esta en la cabeza de la lista
-int eliminarNodoEnPosicionLista (Lista *cabecera, int posicion);
-
-// 11
-// Precondici�n:La lista tiene que ser v�lida
-// Entrada: Una lista
-// Salida: Una copia del nodo 
-// Postcondici�n: Se obtiene una copia del nodo ultimo de la lista
-int leerNodoFinalLista (Lista cabecera);
-=======
-// Postcondici�n: Se obtiene una copia del nodo que esta en la cabeza de la lista
-bool eliminarNodoEnPosicionLista (Lista cabecera, int posicion);
+// Postcondición: Se obtiene una copia del nodo que esta en la cabeza de la lista
+bool eliminarNodoEnPosicionLista (Lista *cabecera, int posicion);
 
 //11
-// Precondici�n:La lista tiene que ser v�lida
+// Precondición:La lista tiene que ser válida
 // Entrada: Una lista
 // Salida: Un boolean(True:Nodo eliminado/False:Nodo no encontrado) 
-// Postcondici�n: Se obtiene una copia del nodo seg�n la posicion
-bool eliminarNodoFinalLista(Lista cabecera);
+// Postcondición: Se obtiene una copia del nodo según la posicion
+bool eliminarNodoFinalLista(Lista *cabecera);
 
 //12
-// Precondici�n:La lista tiene que ser v�lida
+// Precondición:La lista tiene que ser válida
 // Entrada: Una lista
 // Salida: Un boolean(True:Lista destruida) 
-// Postcondici�n: Se obtiene una copia del nodo seg�n la posicion
-bool destruirLista(Lista cabecera);
->>>>>>> 118b1d3368668f6e9747c889f3f415a35bb6b180
+// Postcondición: Se obtiene una copia del nodo según la posicion
+bool destruirLista(Lista *cabecera);
 
 #endif /* LISTA_H */
