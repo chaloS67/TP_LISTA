457352ce3ed29534502f5888c5531fe949eb5725
MatiasFigueroaUDC <matiigfigueroa@gmail.com>
2018-09-13 15:28:34 -0300

    main hecho

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
-/* TDA Lista
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
 
 int main(int argc, char** argv) {
-
+    
+    int opcion;
+    
+     do{
+    puts("---------------MENU---------------");
+    puts("1:Crear lista\n2:Saber si es lista vacia\n3:Insertar nodo al inicio de lista\n4:Insertar nodo en posicion de lista\n5:Insertar nodo final lista\n6:Leer nodo al inicio de lista\n7:Leer nodo en posicion de lista\n8:Leer nodo final lista\n9:Eliminar nodo del inicio de lista\n10:Eliminar nodo en posicion de lista\n11:Eliminar nodo al final de la lista\n12:Destruit lista\n0:SALIR");
+    scanf("%d", &opcion);
+    puts("----------------------------------");
+    switch(opcion){
+    
+    
+    }
+    }while(opcion != 0);
     return (EXIT_SUCCESS);
 }
 
