a34126f5085a298fae1715428dcdfaecfea0a8ce
clarkeen <ngacosta@udc.edu.ar>
2018-09-13 15:31:51 -0300

    main del punto 3.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "LISTA.h"
 
 int main(int argc, char** argv) {
     
@@ -8,7 +8,18 @@ int main(int argc, char** argv) {
     
      do{
     puts("---------------MENU---------------");
-    puts("1:Crear lista\n2:Saber si es lista vacia\n3:Insertar nodo al inicio de lista\n4:Insertar nodo en posicion de lista\n5:Insertar nodo final lista\n6:Leer nodo al inicio de lista\n7:Leer nodo en posicion de lista\n8:Leer nodo final lista\n9:Eliminar nodo del inicio de lista\n10:Eliminar nodo en posicion de lista\n11:Eliminar nodo al final de la lista\n12:Destruit lista\n0:SALIR");
+    puts("1:Crear lista\n"
+            "2:Saber si es lista vacia\n"
+            "3:Insertar nodo al inicio de lista\n"
+            "4:Insertar nodo en posicion de lista\n"
+            "5:Insertar nodo final lista\n"
+            "6:Leer nodo al inicio de lista\n"
+            "7:Leer nodo en posicion de lista\n"
+            "8:Leer nodo final lista\n"
+            "9:Eliminar nodo del inicio de lista\n"
+            "10:Eliminar nodo en posicion de lista\n"
+            "11:Eliminar nodo al final de la lista\n"
+            "12:Destruit lista\n0:SALIR");
     scanf("%d", &opcion);
     puts("----------------------------------");
     switch(opcion){
